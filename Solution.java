017a7e992e7bfb6721381d9be82081616c42cc55
Lachezar Kutsarov <lkutsarov@gmail.com>
2021-09-25 17:51:58 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,27 @@
+
+public class Solution {
+
+    public int searchInsert(int[] nums, int target) {
+
+        int lowerBoundary = 0;
+        int upperBoundary = nums.length - 1;
+        int mid = 0;
+
+        while (lowerBoundary < upperBoundary) {
+
+            mid = (lowerBoundary + upperBoundary) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] < target) {
+                lowerBoundary = mid + 1;
+            }
+            if (nums[mid] > target) {
+                upperBoundary = mid - 1;
+            }
+        }
+
+        return nums[mid] < target ? mid + 1 : mid;
+    }
+
+}
